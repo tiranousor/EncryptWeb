16bb3030f6f762a46f9d6cfb1da6e49b2a0d62ff
Tiranousor <serovakatya2911@gmail.com>
2024-11-02 07:14:13 +0300

    ui

--- src/main/java/com/example/WaterDelivery/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/example/WaterDelivery/config/CustomAuthenticationSuccessHandler.java
@@ -1,18 +1,36 @@
 package com.example.WaterDelivery.config;
 
+import com.example.WaterDelivery.providers.Person;
+import com.example.WaterDelivery.security.PersonDetails;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         String referer = request.getParameter("referer");
-        // По умолчанию перенаправляем на страницу каталога
-        String redirectURL = (referer != null && !referer.isEmpty()) ? referer : "/catalog";
-        response.sendRedirect(redirectURL);
+        // Получаем объект ClientDetails
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        Person person = personDetails.getPerson();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        // Получаем список ролей
+        String roles = authorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+        if (roles.contains("ROLE_ADMIN")) {
+            response.sendRedirect("/admin/orders");
+        } else if (roles.contains("ROLE_USER")) {
+            response.sendRedirect("/catalog");
+        } else {
+            // В случае отсутствия подходящей роли можно направить на страницу ошибки
+            response.sendRedirect("/login?error");
+        }
     }
 }
